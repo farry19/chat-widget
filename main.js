be1ad3fa8aa2acb663c37d2bfafe2a48d697ff1c
Sheraz <hsherazch321@gmail.com>
2023-11-22 14:05:12 +0500

    URL Updated

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js";
 import { styles, CLOSE_ICON, MESSAGE_ICON } from "./assets.js";
 
 // const ENDPOINT = `http://localhost:8080`
-const ENDPOINT = `http://ec2-34-199-62-141.compute-1.amazonaws.com:8080`;
+const ENDPOINT = `//ec2-52-21-164-16.compute-1.amazonaws.com:8080`;
 const API_ENDPOINT = `${ENDPOINT}/api/v1/widget-settings`;
 const WS_ENDPOINT = `${ENDPOINT}`;
 
